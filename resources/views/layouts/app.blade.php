0a5fbc44d2ff3881839764567f8c8d1ee802ce6d
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2022-05-02 04:39:10 +0500

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link active" href="{{ route('home') }}">{{ __('Business Metrics List') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
