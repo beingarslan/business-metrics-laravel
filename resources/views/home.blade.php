127a95f424cad79d98a9c1720e39dc5ea971438b
ARSLAN ASLAM <beingfastian@gmail.com>
2022-06-10 21:23:36 +0500

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,6 +45,51 @@
                             <small>Return on equity = net profit/shareholder's equity</small>
                         </a>
 
+                        <a href="/business-metrics/debt-to-equity-ratio" class="list-group-item list-group-item-action" aria-current="true">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">Debt-to-equity ratio</h5>
+                                <!-- <small>3 days ago</small> -->
+                            </div>
+                            <p class="mb-1">The debt-to-equity equation is a business formula that shows how much debt a company uses to finance its operations.
+                                Total liabilities include all of the costs you must pay to outside parties, such as accounts payable, balances, interest, and principal payments on debt.
+                                Total equity is how much of the company actually belongs to the owners. In other words, it’s the amount of money the owner has invested in his or her own company.</p>
+                            <small>Debt-to-equity ratio = total liabilities ÷ total equity</small>
+                        </a>
+
+                        <a href="/business-metrics/return-on-investment" class="list-group-item list-group-item-action" aria-current="true">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">Return on investment</h5>
+                                <!-- <small>3 days ago</small> -->
+                            </div>
+                            <p class="mb-1">Do you make wise investments? How can you tell?
+                                You make wise investments when you gain more than what you paid. To find out how well you are investing your business’s money, find your return on investment (ROI) percentage.</p>
+                            <small>Return on investment = (Net Return on Investment / Cost of Investment) × 100</small>
+                        </a>
+
+                        <a href="/business-metrics/inventory-shrinkage" class="list-group-item list-group-item-action" aria-current="true">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">Inventory Shrinkage</h5>
+                                <!-- <small>3 days ago</small> -->
+                            </div>
+                            <p class="mb-1">Why you should know the inventory shrinkage formula
+                                If you are seeing an increase in inventory shrinkage, there’s a good chance that someone is stealing from you. There is inventory listed in your accounting records that isn’t found in your actual inventory.
+                                Inventory shrinkage could also result from miscounting, not using the right units of measure, or unclear cooperation with third parties. For example, your supplier could charge you for 100% of the goods you requested but only deliver 96% of them.
+                            </p>
+                            <small>Inventory Shrinkage = [(Recorded Inventory – Actual Inventory) / Recorded Inventory] X 100</small>
+                        </a>
+
+                        <a href="/business-metrics/break-even-point" class="list-group-item list-group-item-action" aria-current="true">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">Break-even Point</h5>
+                                <!-- <small>3 days ago</small> -->
+                            </div>
+                            <p class="mb-1">It’s a point at which a company’s expenses are exactly covered by its sales.
+                                In other words, it’s a production level where the total expenses are exactly the same as the total revenue for a product. <br>
+                                Calculating the break-even point is a crucial financial analysis tool for business owners. A small business owner can use this formula to find out how many units of products they need to sell to cover production costs.
+                            </p>
+                            <small>Break-even Point = Fixed Costs / (Sales Price Per Unit – Variable Costs Per Unit)</small>
+                        </a>
+
                     </div>
                 </div>
             </div>
