61142049ae026fe1b4e9e8cb236fcedca665e288
ARSLAN ASLAM <beingfastian@gmail.com>
2022-06-11 01:31:08 +0500

    break-even-point

--- app/Http/Livewire/BusinessMetrics/BreakEvenPoint.php
+++ app/Http/Livewire/BusinessMetrics/BreakEvenPoint.php
@@ -6,6 +6,23 @@ use Livewire\Component;
 
 class BreakEvenPoint extends Component
 {
+    public $sales_price_per_unit;
+    public $fixed_costs;
+    public $variable_costs_per_unit;
+    public $breakEvenPoint;
+    public $results = false;
+
+
+    public function getBreakEvenPoint(){
+        $this->validate([
+            'fixed_costs' => 'required|numeric|min:0',
+            'sales_price_per_unit' => 'required|numeric|min:1',
+            'variable_costs_per_unit' => 'required|numeric|min:0',
+        ]);
+
+        $this->breakEvenPoint = ($this->fixed_costs / $this->sales_price_per_unit) - $this->variable_costs_per_unit;
+        $this->results = true;
+    }
     public function render()
     {
         return view('livewire.business-metrics.break-even-point');
--- resources/views/livewire/business-metrics/break-even-point.blade.php
+++ resources/views/livewire/business-metrics/break-even-point.blade.php
@@ -1,3 +1,64 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Break-even Point') }}
+            <!-- <button class="btn btn-secondary ">Back</button> -->
+        </div>
+
+        <div class="card-body">
+            <!-- Debt to Equity Ratio form -->
+            <form wire:submit.prevent="getBreakEvenPoint">
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Fixed Costs</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Fixed Costs" wire:model="fixed_costs">
+                    @error('fixed_costs')
+                    <span class="text-danger" role="alert">
+                        {{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Sales Price Per Unit</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Sales Price Per Unit" wire:model="sales_price_per_unit">
+                    @error('sales_price_per_unit')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+               
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Variable Costs Per Unit</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Variable Costs Per Unit" wire:model="variable_costs_per_unit">
+                    @error('variable_costs_per_unit')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-secondary">Calculate</button>
+                <div wire:loading class="text-warning">
+                    Calculating...
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- results -->
+    @if($results)
+    <div class="card mt-2">
+        <div class="card-header">
+            {{ __('Break-even Point Results') }}
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    Result: {{ $breakEvenPoint }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 </div>
