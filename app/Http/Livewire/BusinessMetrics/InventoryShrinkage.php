c301dcacb6aca59449b5657b11b2917b56f94530
ARSLAN ASLAM <beingfastian@gmail.com>
2022-06-11 01:40:09 +0500

    inventory-shrinkage

--- app/Http/Livewire/BusinessMetrics/InventoryShrinkage.php
+++ app/Http/Livewire/BusinessMetrics/InventoryShrinkage.php
@@ -5,7 +5,24 @@ namespace App\Http\Livewire\BusinessMetrics;
 use Livewire\Component;
 
 class InventoryShrinkage extends Component
-{
+{    
+    public $recorded_inventory;
+    public $actual_inventory;
+    public $inventoryShrinkage;
+    public $results = false;
+
+    public function getInventoryShrinkage()
+    {
+        $this->validate([
+            'recorded_inventory' => 'required|numeric|min:1',
+            'actual_inventory' => 'required|numeric|min:0',
+        ]);
+
+        $this->inventoryShrinkage = (($this->recorded_inventory - $this->actual_inventory) / $this->recorded_inventory) * 100;
+        $this->results = true;
+    }
+
+
     public function render()
     {
         return view('livewire.business-metrics.inventory-shrinkage');
--- resources/views/livewire/business-metrics/inventory-shrinkage.blade.php
+++ resources/views/livewire/business-metrics/inventory-shrinkage.blade.php
@@ -1,3 +1,54 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Inventory Shrinkage') }}
+            <!-- <button class="btn btn-secondary ">Back</button> -->
+        </div>
+
+        <div class="card-body">
+            <!-- Gross Profit Margin form -->
+            <form wire:submit.prevent="getInventoryShrinkage">
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Recorded Inventory</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Recorded Inventory" wire:model="recorded_inventory">
+                    @error('recorded_inventory')
+                    <span class="text-danger" role="alert">
+                        {{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Actual Inventory</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Actual Inventory" wire:model="actual_inventory">
+                    @error('actual_inventory')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-secondary">Calculate</button>
+                <div wire:loading class="text-warning">
+                    Calculating...
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- results -->
+    @if($results)
+    <div class="card mt-2">
+        <div class="card-header">
+            {{ __('Inventory Shrinkage Results') }}
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    Result: {{ $inventoryShrinkage }}%
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 </div>
