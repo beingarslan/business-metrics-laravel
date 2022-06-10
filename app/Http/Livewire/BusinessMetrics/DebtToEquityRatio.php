291d79fc64397417211e10fc79ae05d2721dcab6
ARSLAN ASLAM <beingfastian@gmail.com>
2022-06-11 01:22:47 +0500

    debt-to-equity-ratio

--- app/Http/Livewire/BusinessMetrics/DebtToEquityRatio.php
+++ app/Http/Livewire/BusinessMetrics/DebtToEquityRatio.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class DebtToEquityRatio extends Component
 {
+    public $total_liabilities;
+    public $total_equity;
+    public $debttoEquityRatio;
+    public $results = false;
+
+    public function getDebttoEquityRatio()
+    {
+        $this->validate([
+            'total_liabilities' => 'required|numeric|min:0',
+            'total_equity' => 'required|numeric|min:1',
+        ]);
+
+        $this->debttoEquityRatio = ($this->total_liabilities / $this->total_equity) ;
+        $this->results = true;
+    }
+
     public function render()
     {
         return view('livewire.business-metrics.debt-to-equity-ratio');
--- resources/views/livewire/business-metrics/debt-to-equity-ratio.blade.php
+++ resources/views/livewire/business-metrics/debt-to-equity-ratio.blade.php
@@ -1,3 +1,54 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <div class="card">
+        <div class="card-header">
+            {{ __('Debt to Equity Ratio') }}
+            <!-- <button class="btn btn-secondary ">Back</button> -->
+        </div>
+
+        <div class="card-body">
+            <!-- Debt to Equity Ratio form -->
+            <form wire:submit.prevent="getDebttoEquityRatio">
+
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Total Liabilities</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Total Liabilities" wire:model="total_liabilities">
+                    @error('total_liabilities')
+                    <span class="text-danger" role="alert">
+                        {{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Total Equity</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Total Equity" wire:model="total_equity">
+                    @error('total_equity')
+                    <span class="text-danger" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-secondary">Calculate</button>
+                <div wire:loading class="text-warning">
+                    Calculating...
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <!-- results -->
+    @if($results)
+    <div class="card mt-2">
+        <div class="card-header">
+            {{ __('Debt to Equity Ratio Results') }}
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-6">
+                    Result: {{ $debttoEquityRatio }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 </div>
